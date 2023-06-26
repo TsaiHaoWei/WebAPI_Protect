22ef8aa8cff3c901fa39d4c15fb4251ddbeeb0db
澔緯 蔡 <tony87119123@gmail.com>
2023-06-26 14:56:55 +0800

    test

--- MyWebAPI/DataBase/DBUtility.cs
+++ MyWebAPI/DataBase/DBUtility.cs
@@ -28,6 +28,7 @@ namespace MyWebAPI.DataBase
             sqlBuilder.InitialCatalog = DBSetup()["Database"];
             sqlBuilder.UserID = DBSetup()["LogId"];
             sqlBuilder.Password = DBSetup()["LogPass"]; 
+            test
             sqlBuilder.ApplicationName = "MVCDataBase";
             SysConnectString = sqlBuilder.ToString();
 
